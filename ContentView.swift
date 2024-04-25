fb9d3624014c0360ffbbb8b5a3692354895605a4
Christian <Cdzhokov6094@stu.d214.org>
2024-04-25 15:12:53 -0500

    made navigation button

--- ContentView.swift
+++ ContentView.swift
@@ -3,41 +3,46 @@ import SwiftUI
 struct ContentView: View {
     
     @Binding var gotonext:NextPage
-
+    
     
     var body: some View {
-    
         
-        VStack(spacing:60){
-            
+        NavigationStack{
+            VStack(spacing:60){
+                
                 Text("Leaf-Budget Smartly")
                 Text("Budget Without The Worry of Overspending")
                 Text("Welcome")
                 
-               
-            
-            TextField("enter budget here", text: $gotonext.Budget)
-            
-            
-
+                
+                
+                TextField("enter budget here", text: $gotonext.Budget)
+                
+                NavigationLink("Budget"){
+                    HistoryView()
+                }
+                
+                
+                
+                
                 Image(systemName: "rectangle.fill")
-                .scaleEffect(CGSize(width: 40.0, height: 6.0))
-                .offset(x: 0, y: 370)
-                .foregroundColor(.green)
-            Image(systemName: "circle.fill")
-                .scaleEffect(CGSize(width: 7.0, height: 6.0))
-                .offset(x: 0, y: 260)
-                .foregroundColor(.green)
+                    .scaleEffect(CGSize(width: 40.0, height: 6.0))
+                    .offset(x: 0, y: 370)
+                    .foregroundColor(.green)
+                Image(systemName: "circle.fill")
+                    .scaleEffect(CGSize(width: 7.0, height: 6.0))
+                    .offset(x: 0, y: 260)
+                    .foregroundColor(.green)
                 Image(systemName: "house.circle")
                     .scaleEffect(CGSize(width: 6, height: 6))
                     .offset(x: 0, y: 200)
                 Image(systemName: "folder.fill")
-                .scaleEffect(CGSize(width:3.7, height: 3.7))
+                    .scaleEffect(CGSize(width:3.7, height: 3.7))
                     .offset(x: -130, y: 130)
                 Image(systemName: "person.circle.fill")
-                .scaleEffect(CGSize(width: 4, height: 4))
-                .offset(x: 130, y: 55)
+                    .scaleEffect(CGSize(width: 4, height: 4))
+                    .offset(x: 130, y: 55)
             }
         }
     }
-
+}
--- History.swift
+++ History.swift
@@ -1,8 +1,12 @@
 import SwiftUI
 
 struct HistoryView: View {
+    
     var body: some View {
-        VStack {
+        
+       
+        
+        VStack{
             Text("Budget spent on")
                 .frame(width: 100, height: 100, alignment: .trailing)
             
