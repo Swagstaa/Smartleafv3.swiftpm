aa3baaf7064b7a009549fd3544eb4ee694b983d2
Christian <Cdzhokov6094@stu.d214.org>
2024-04-29 14:17:38 -0500

    created navigation button on profile view

--- profile.swift
+++ profile.swift
@@ -5,9 +5,13 @@ struct ProfileView: View {
     @State var budget = 0
     
     var body: some View {
-        VStack {
+        
+        NavigationStack {
              Text("you have spent out of \(budget)")
                 .frame(width: 100, height: 100, alignment: .trailing)
+            NavigationLink("History"){
+               HistoryView()
+            }
             
            }
         }
