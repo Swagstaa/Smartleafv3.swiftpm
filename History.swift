486fff2a8ba787ef839fde96845cd9c2d02f1b39
Christian <Cdzhokov6094@stu.d214.org>
2024-05-01 14:11:49 -0500

    made text for profile and history view

--- History.swift
+++ History.swift
@@ -4,9 +4,10 @@ struct HistoryView: View {
     
     var body: some View {
         
-       
-        
         VStack{
+            Text("Profile")
+                .font(.largeTitle)
+                .fontWeight(.bold)
             Text("You have spent () out ()")
             Text("You can spend () more and still stay in budget")
             
--- profile.swift
+++ profile.swift
@@ -7,6 +7,9 @@ struct ProfileView: View {
     var body: some View {
         
         NavigationStack {
+            Text("Budget History")
+                .font(.largeTitle)
+                .fontWeight(.bold)
              Text("you have spent out of \(budget)")
                 .frame(width: 100, height: 100, alignment: .trailing)
             NavigationLink("History") {
