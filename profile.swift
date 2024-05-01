cac3ca17c30e9b516d527eb7b1e74b1825f8f219
Allan Sulev <asulev7401@stu.d214.org>
2024-05-01 14:48:32 -0500

    Put Taskbar into profile page.

--- profile.swift
+++ profile.swift
@@ -14,6 +14,27 @@ struct ProfileView: View {
                 .frame(width: 100, height: 100, alignment: .trailing)
             NavigationLink("History") {
                 HistoryView()
+                
+                
+                
+                
+                Image(systemName: "rectangle.fill")
+                    .scaleEffect(CGSize(width: 40.0, height: 6.0))
+                    .offset(x: 0, y: 350)
+                    .foregroundColor(.green)
+                Image(systemName: "circle.fill")
+                    .scaleEffect(CGSize(width: 7.0, height: 6.0))
+                    .offset(x: 0, y: 300)
+                    .foregroundColor(.green)
+                Image(systemName: "house.circle")
+                    .scaleEffect(CGSize(width: 6, height: 6))
+                    .offset(x: 0, y: 300)
+                Image(systemName: "folder.fill")
+                    .scaleEffect(CGSize(width:3.7, height: 3.7))
+                    .offset(x: -130, y: 290)
+                Image(systemName: "person.circle.fill")
+                    .scaleEffect(CGSize(width: 4, height: 4))
+                    .offset(x: 130, y: 275)
             }
             
            }
