261ffeeeda00d219fce4ffbbf7fcd90909e5acc3
Karim <kramirezchina7337@stu.d214.org>
2024-05-03 14:45:16 -0500

    Made "budget spent on" text in history view

--- History.swift
+++ History.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 struct HistoryView: View {
     
+    @State var category = ""
+    
     var body: some View {
         
         VStack{
@@ -11,6 +13,17 @@ struct HistoryView: View {
             Text("You have spent () out ()")
             Text("You can spend () more and still stay in budget")
             
+            
+            
+            Text("Budget spent on: \(category)")
+            
+            
+            
             }
         }
     }
+
+
+
+
+
