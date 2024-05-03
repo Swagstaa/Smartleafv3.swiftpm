a55bb043075377674ed822c4b63490a472507099
Allan Sulev <asulev7401@stu.d214.org>
2024-05-03 14:24:27 -0500

    Taskbar = deleted

--- ContentView.swift
+++ ContentView.swift
@@ -30,24 +30,6 @@ struct ContentView: View {
                 
                 
                 
-                
-                Image(systemName: "rectangle.fill")
-                    .scaleEffect(CGSize(width: 40.0, height: 6.0))
-                    .offset(x: 0, y: 370)
-                    .foregroundColor(.green)
-                Image(systemName: "circle.fill")
-                    .scaleEffect(CGSize(width: 7.0, height: 6.0))
-                    .offset(x: 0, y: 260)
-                    .foregroundColor(.green)
-                Image(systemName: "house.circle")
-                    .scaleEffect(CGSize(width: 6, height: 6))
-                    .offset(x: 0, y: 200)
-                Image(systemName: "folder.fill")
-                    .scaleEffect(CGSize(width:3.7, height: 3.7))
-                    .offset(x: -130, y: 130)
-                Image(systemName: "person.circle.fill")
-                    .scaleEffect(CGSize(width: 4, height: 4))
-                    .offset(x: 130, y: 55)
             }
         }
     }
