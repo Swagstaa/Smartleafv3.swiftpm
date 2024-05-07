a0cd453c3bd21814c01922361a2ad7e8e561e61f
Karim <kramirezchina7337@stu.d214.org>
2024-05-07 14:48:42 -0500

    made a selectable button in history view

--- History.swift
+++ History.swift
@@ -2,18 +2,31 @@ import SwiftUI
 
 struct HistoryView: View {
     
-    @State var category = ""
+    let options = ["Clothes","Groceries","Necessities"]
+    @State private var categorySelect = 0
     
     var body: some View {
-        
         VStack{
-
-            Text("You spent your budget on:\(category)")
             
+            Text("Selected category: \(options[categorySelect])")
+                .padding()
+                .bold()
+            
+            Picker("Select a category", selection: $categorySelect) {
+                ForEach(0..<options.count) { index in
+                    Text(self.options[index])
+                }
+                .bold()
             }
+            
+            .pickerStyle(DefaultPickerStyle())
+            .labelsHidden()
+            .padding()
+            
+            
         }
     }
-
+}
 
 
 
