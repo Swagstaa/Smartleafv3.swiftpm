d6555a97e9060cb452a8989518f59b0df9130724
Tyler Wolff <twolff6463@stu.d214.org>
2024-05-15 14:38:16 -0500

    fixed budget

--- profile.swift
+++ profile.swift
@@ -4,41 +4,18 @@ struct ProfileView: View {
     
     let budget: NextPage
     
-    @State var spenttextfield = ""
-    
-    @State var gotonext = NextPage(Budget: "", spent: 0)
-    
     var body: some View {
         
         NavigationStack {
             Text("Budget History")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-            
-            
-            
-            VStack(spacing:30){
-                TextField("Enter Budget", text: $gotonext.Budget)
-                
-                TextField("Enter Money Spent", text: $spenttextfield)
-                
-            }
-            
-            Text("")
-            Text("")
-            VStack(spacing:50){
-                Button("Add Spent Amount") {
-                    gotonext.spent = Int(spenttextfield) ?? 0
-                    
-                }
+                .foregroundColor(.gray)
+            Text("you have spent \(budget.spent) out of \(budget.Budget)")
                
-                Text("you have spent \(budget.spent) out of \(budget.Budget)")
-                
-                
-                
-            }
+           
+            
+           }
         }
     }
-    
-}
 
