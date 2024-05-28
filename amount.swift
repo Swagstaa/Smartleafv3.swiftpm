e9046d1470dde0f60c31a2ddc9f5e8d4971605a5
Karim <kramirezchina7337@stu.d214.org>
2024-05-28 14:53:13 -0500

    made texts visually appealing to the audience(made it green and labeled)

--- amount.swift
+++ amount.swift
@@ -3,9 +3,16 @@ import SwiftUI
 struct AmountView: View {
     var body: some View {
         VStack{
+            Text("Amount")
+                .bold()
+                .font(.title2)
+                .padding()
+                .foregroundColor(.gray)
           
-         Text("You this amount left from your budget")
-            
+         Text("You have this amount left from your budget")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.green)
             
         }
     }
